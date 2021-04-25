1519e3f29721b045c91b4eb42dd842dc5fc36363
Савелий Крендельков <savely@Savelijs-MacBook-Air.local>
2021-04-25 21:43:40 +0300

    solved some tasks

--- tribonacci2.hs
+++ tribonacci2.hs
@@ -1,5 +1,15 @@
-tribonacciNum :: Num a => a -> a -> a -> Int -> a
-tribonacci _ _ c 0 = c
-tribonacciNum a b c n = tribonacci b c (a + b + c) (n - 1)
+tribonacciHelper :: Num a => (a, a, a) -> [a] -> Int -> Int -> [a]
+tribonacciHelper (a, b, c) list n len
+        | n == len      = list
+        | otherwise     = tribonacciHelper (b, c, a + b + c) (list ++ [c]) (n + 1) len
 
-tribonacciHelper :: 
+tribonacci :: Num a => (a, a, a) -> Int -> [a]
+tribonacci _ 0 = []
+tribonacci (a, _, _) 1 = [a]
+tribonacci (a, b, c) 2 = [a, b]
+tribonacci (a, b, c) n = a:b:tribonacciHelper (a, b, c) [] 0 (n - 2)
+
+-- это то же самое, что ты делал с матрицами. Абсолютно. Конкретно в данном случае умножение матриц бессмысленно.
+
+main :: IO ()
+main = print (tribonacci (1, 1, 1) 5)
