22581ef2f8b18d786ba8ab4d5d38f37d8068b57a
Савелий Крендельков <savely@Savelijs-MacBook-Air.local>
2021-05-06 00:18:01 +0300

    resolved almost everything

--- parsePerson.hs
+++ parsePerson.hs
@@ -17,20 +17,26 @@ data Person =
 
 parsePerson :: String -> Either Error Person
 parsePerson s
-  | any (not . checkParse) lineS = Left ParsingError
+  | any (not . checkParse) lineS || null s = Left ParsingError
   | not allFields = Left IncompleteDataError
-  | not allNum = Left (IncorrectDataError ageValue)
+  | not allNum = Left $ IncorrectDataError ageValue
   | otherwise =
     Right
       (Person
          {firstName = firstNameValue, lastName = lastNameValue, age = ageInt})
   where
-    checkParse s = (unwords . words $ s) == s
+    label line = takeWhile (\x -> x /= ' ') line
+    value line = drop 3 . dropWhile (\x -> x /= ' ') $ line
+    checkParse s =
+      let lab = label s
+          val = value s
+       in lab ++ " = " ++ val == s && (not . null $ val) && (not . null $ lab)
     lineS = lines s
     completeList = ["age", "firstName", "lastName"]
-    wordS = map words lineS
-    assocS = map (\x -> (head x, last x)) wordS
-    allFields = ((map fst assocS) \\ completeList) == []
+    assocS = map (\x -> (label x, value x)) lineS
+    allFields =
+      let lst = map fst assocS
+       in and (foldr (\x acc -> (x `elem` lst) : acc) [] completeList)
     ageValue = snd . head . filter (\(x, _) -> x == "age") $ assocS
     lastNameValue = snd . head . filter (\(x, _) -> x == "lastName") $ assocS
     firstNameValue = snd . head . filter (\(x, _) -> x == "firstName") $ assocS
