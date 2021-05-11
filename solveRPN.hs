46ae2e5bf7b06541b938ae7fbe05c5037cd022d1
Marg Jacqualine <mjacqual@il-k1.msk.21-school.ru>
2021-05-11 15:25:26 +0300

    becoming haskell

---
+++ solveRPN.hs
@@ -0,0 +1,12 @@
+import           Data.List
+
+solveRPN :: String -> Double
+solveRPN = head . foldl f [] . words
+  where
+    f (x:y:ys) "*"    = (x * y) : ys
+    f (x:y:ys) "+"    = (x + y) : ys
+    f (x:y:ys) "-"    = (y - x) : ys
+    f (x:y:ys) "^"    = (y ** x) : ys
+    f (x:xs) "ln"     = log x : xs
+    f xs "sum"        = [sum xs]
+    f xs numberString = read numberString : xs
