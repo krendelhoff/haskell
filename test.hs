0b9f025099f74ee7f817b6543a7c119807a2ed0b
Marg Jacqualine <mjacqual@il-k1.msk.21-school.ru>
2021-05-13 11:58:34 +0300

    becoming haskell

--- test.hs
+++ test.hs
@@ -10,10 +10,16 @@ safeHead (x:_) = Just x
 main :: IO ()
 main = do
   print (f "wow")
--- он не выдал ошибку и вывел тип
   print ((\(Just a) -> a) (Just 5))
---  print (safeHead [])
     -- выдает ошибку, т.к. это обозначение для пустого списка любого типа
     -- но возвращает то он Maybe a, то есть тип конкретный, если не указать, то там ambiguous
   print (safeHead ([] :: [Int]))
     -- так же обсолютно очев. почему если derive read, то всегда надо явно указывать - откуда он знает, надо считать просто строку, или там конструктор?"
+    --
+
+-- он не выдал ошибку и вывел тип
+--  print (safeHead [])
+instance Functor IO where
+  f `fmap` boba = do
+    x <- boba
+    return $ f x
