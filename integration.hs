066f67ae2f33566b9d90f892297be531e3338401
Marg Jacqualine <mjacqual@il-k1.msk.21-school.ru>
2021-05-14 17:05:17 +0300

    becoming haskell

--- integration.hs
+++ integration.hs
@@ -1,16 +1,18 @@
-
 integration :: (Double -> Double) -> Double -> Double -> Double
 integration f a b
-    | a == b    = 0
-    | otherwise = sum $ map (\section -> trapezoidArea f section) (makeFragmentation (fragmentate a b) [])
-                   where
-                        fragmentate a b = [a, (a + (b - a) / 1000)..b]
-                        makeFragmentation list acc
-                            | null list || null (tail list)     = acc
-                            | otherwise     = makeFragmentation (drop 1 list) ((take 2 list):acc)
-                        trapezoidArea f (x:y:[]) = ((f y) + (f x)) * (y - x) / 2
+  | a == b = 0
+  | otherwise =
+    sum $
+    map
+      (\section -> trapezoidArea f section)
+      (makeFragmentation (fragmentate a b) [])
+  where
+    fragmentate a b = [a,(a + (b - a) / 1000) .. b]
+    makeFragmentation list acc
+      | null list || null (tail list) = acc
+      | otherwise = makeFragmentation (drop 1 list) ((take 2 list) : acc)
+    trapezoidArea f (x:y:[]) = ((f y) + (f x)) * (y - x) / 2
 
 main :: IO ()
 main = do
-    print (integration sin pi 0)
-
+  print (integration sin pi 0)
