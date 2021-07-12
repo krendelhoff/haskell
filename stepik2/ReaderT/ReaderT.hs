c130609f483b7c7ca2069a52f278d316403a1afb
Савелий Крендельков <savely@Savelijs-MacBook-Air.local>
2021-07-12 17:18:56 +0300

    finished ReaderT

--- stepik2/ReaderT/ReaderT.hs
+++ stepik2/ReaderT/ReaderT.hs
@@ -21,3 +21,30 @@ instance Applicative m => Applicative (ReaderT r m) where
   -------------------- ReaderT $ liftA2 (<*>) (runReader f) (runReaderT ax)
   -- смысл - слезаем внутрь внешнего контекста и там аплликатируем
   -- это в точности одно и то же
+  --
+
+instance Monad m => Monad (ReaderT r m) where
+  return = pure
+  ma >>= f =
+    ReaderT $ \r ->
+      let bob = runReaderT m r
+       in bob >>= (\a -> runReaderT (f a) r)
+       -- m >>= k = Reader $ \r -> let v = runReader m r
+       --                          in runReader (k v) r
+       --      то же самое дословно
+       --      только теперь мы выполняем еще один эффект
+       --
+       --
+       --      ReaderT $ \env -> do    -- вычисление в абстрактной
+       --               v <- runReaderT m r       внутренней монаде
+       --               runReaderT (f a) r
+       --
+
+ask :: Monad m => ReaderT r m r
+ask = asks id
+
+asks :: Monad m => (r -> a) -> ReaderT r m a
+asks = reader
+
+local :: Monad m => (r -> r) -> Reader r m a -> Reader r m a
+local f rdr = ReaderT (runReaderT rdr . f)
